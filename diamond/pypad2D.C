46e2a45e10c474277a7796df919d2dde5aa5070e
Christian Dorfer <dorfer@mutter.ethz.ch>
2017-09-13 17:54:53 +0200

    status wednesday

--- diamond/pypad2D.C
+++ diamond/pypad2D.C
@@ -5,18 +5,18 @@ KDetector  det;
 det.SetDriftHisto(15e-9);
 
 // Detector binning
-det.nx = 450;
-det.ny = 100;
-det.nz = 1;
+Int_t nx = 460;
+Int_t ny = 50;
+Int_t nz = 1;
 
-// Detector dimensions
-Float_t dimX = 4500;
-Float_t dimY = 510;
-Float_t dimZ = 1;
+// Detector dimensions in micrometer
+Int_t dimX = 4600;
+Int_t dimY = 500;
+Int_t dimZ = 1;
 
 // Pad sizes
 Float_t pDimX = 2000;
-Float_t pDimY = 0.1;
+Float_t pDimY = 1;
 Float_t pDimZ = 1;
 
 //Pad position
@@ -27,31 +27,31 @@ Float_t pYhigh = dimY;
 
 // Material constants
 Int_t diamond = 10;
-Int_t aluminum = 100; //actually we use Cr/Au but that should not matter
+Int_t aluminum = 100; //=Al, actually we use Cr/Au but that should not matter
 
 // Space charge
-Float_t space_charge = -0.25; //equivalent to a 2.5e11 space charge
+Float_t space_charge = -0.5; //equivalent to a 5e11 space charge
 
 // Bias voltages
-det.Voltage = 400;
+det.Voltage = -400;
 
 // Electrode setup
-Int_t gndBit = 2; //defines ground electrode
-Int_t padBit = 16385; // bit2 = 1 (HV electrode), bit14 = 1 (electrode for which Ramo field is calculated)
+Int_t gndBit = 1; //defines ground electrode
+Int_t padBit = 16386; // bit2 = 1 (HV electrode), bit14 = 1 (electrode for which Ramo field is calculated)
 
 // MIP properties
-Float_t entryPointX = 2000;
-Float_t entryPointY = 100;
-Float_t entryPointZ = 0;
-Float_t extX = 40;
-Float_t extY = 0;
-Float_t extZ = 1;
+Int_t entryPointX = 300;
+Int_t entryPointY = 300;
+Int_t entryPointZ = 0;
+Int_t extX = 100;
+Int_t extY = 0;
+Int_t extZ = 1;
 
 // Diffusion
 det.diff=1;
 
 // Output directory
-const char *currentProfile = "../Results/current_profile.dat";
+const char *currentProfile="../Results/prof_y=450.dat";
 
 // Other
 Bool_t drawing = true;
@@ -59,36 +59,40 @@ Bool_t drawing = true;
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //  Sensor geometry
-det.EG = new TH3I("EG","EG",det.nx,0,dimX, det.ny,0,dimY, det.nz,0,dimZ); 
+det.EG = new TH3I("EG","EG", nx,0,dimX, ny,0,dimY, nz,0,dimZ); 
 det.EG->GetXaxis()->SetTitle("x [#mum]"); 
 det.EG->GetYaxis()->SetTitle("y [#mum]"); 
 det.EG->GetZaxis()->SetTitle("z [#mum]");
 
 //  Detector material
-det.DM = new TH3I("DM","DM",det.nx,0,dimX, det.ny,0,dimY, det.nz,0,dimZ);
+det.DM = new TH3I("DM","DM", nx,0,dimX, ny,0,dimY, nz,0,dimZ); 
 det.DM->GetXaxis()->SetTitle("x [#mum]"); 
 det.DM->GetYaxis()->SetTitle("y [#mum]"); 
 det.DM->GetZaxis()->SetTitle("z [#mum]");
 
 //  Space charge
-det.NeffH = new TH3F("Neff","Neff",det.nx,0,dimX, det.ny,0,dimY, det.nz,0,dimZ);
+det.NeffH = new TH3F("Neff","Neff", nx,0,dimX, ny,0,dimY, nz,0,dimZ); 
 det.NeffH->GetXaxis()->SetTitle("x [#mum]");
 det.NeffH->GetYaxis()->SetTitle("y [#mum]");
 det.NeffH->GetZaxis()->SetTitle("z [#mum]");
 
+
+
+std::cout << "pad bin location: " << det.EG->GetXaxis()->FindBin(pXpos) << std::endl;
+
 //  Material and space charge setup:
 Int_t i, j, k;
-for(int k=1; k<=det.nz; k++) 
-  for(int j=1; j<=det.ny; j++)
-    for(int i=1; i<=det.nx; i++) 
+for(int k=1; k<=nz; k++) 
+  for(int j=1; j<=ny; j++)
+    for(int i=1; i<=nx; i++) 
       {
-	det.DM->SetBinContent(i, j, k, diamond);
-	det.NeffH->SetBinContent(i, j, k, space_charge);
+		det.DM->SetBinContent(i, j, k, diamond);
+		//det.NeffH->SetBinContent(i, j, k, space_charge);
       }
 
 //  GND Pad
 Float_t GndPos[3]={pXpos, pYlow, pZpos}; 
-Float_t GndSiz[3]={pDimX, pDimY, pDimZ};
+Float_t GndSiz[3]={pDimX, pDimY, pDimZ}; 
 det.ElRectangle(GndPos, GndSiz, gndBit, aluminum);
 
 //  Top Pad
@@ -110,34 +114,43 @@ if(drawing){
 	TCanvas *c1 = new TCanvas("c1","c1",1200,400);
 	c1->cd();
 	TH2 *hEFxy = det->Draw("EFxy");
-	hEFxy->GetYaxis()->SetRangeUser(20,500);
-	//hEFxy->GetXaxis()->SetRangeUser(20,4480);
+	//hEFxy->GetYaxis()->SetRangeUser(10,480);
+	//hEFxy->GetXaxis()->SetRangeUser(20,4580);
 	hEFxy->GetZaxis()->SetTitle("Electric Field [V/#mum]");
-	hEFxy->SetTitle("4.5x4.5mm Diamond Pad Detector");
+	hEFxy->SetTitle("Electric Field");
 	c1->SetRightMargin(0.15);
-
 	hEFxy->Draw("COLZ");
 
-	TCanvas *c2 = new TCanvas("c2","c2");
+	TCanvas *c2 = new TCanvas("c2","c2",1200,400);
 	c2->cd();
+	TH2 *hPxy = det->Draw("P");
+	hPxy->GetZaxis()->SetTitle("Potential [V]");
+	hPxy->SetTitle("Potential");
+	c2->SetRightMargin(0.15);
+	hPxy->Draw("COLZ");
+
+	TCanvas *c3 = new TCanvas("c3","c3");
+	c3->cd();
 	TH1 *projy = hEFxy->ProjectionY("",50, 50);
 	projy.Draw();
 
-	TCanvas *c3 = new TCanvas("c3","c3", 1200,400);
-	c3->cd();
+	TCanvas *c4 = new TCanvas("c4","c4", 1200,400);
+	c4->cd();
 	det.ShowMipIR(100);
 
-	TCanvas *c4 = new TCanvas("c4","c4");
-	c4->cd();
+	TCanvas *c5 = new TCanvas("c5","c5");
+	c5->cd();
 	TH1 *charge = det.sum; //draw current
 
 	charge->GetXaxis()->SetRangeUser(-5e-9,10e-9);
 	charge->Draw();
+	det.neg.Draw("SAME"); 
+	det.pos.Draw("SAME");
+	charge->Draw("SAME");
 }
 
 //write current profile to file for later analysis
 std::ofstream ofs (currentProfile, std::ofstream::out);
-ofs << "MIP infos" << std::endl;
 TAxis *axis = charge->GetXaxis();
 for(bin=0; bin<charge->GetNbinsX(); bin++){
 	Double_t binCenter = axis->GetBinCenter(bin);
